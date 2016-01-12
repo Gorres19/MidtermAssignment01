b2875547641838e636202c13822cce3ca63ae8cd
gorres19 <gorresjoshua15@gmail.com>
2016-01-12 16:54:46 +0800

    unfinished assignment, Implemented auto complete text view and countries that can be searched already

--- app/src/main/java/sidespell/tech/midtermassignment01/MainActivity.java
+++ app/src/main/java/sidespell/tech/midtermassignment01/MainActivity.java
@@ -1,15 +1,27 @@
 package sidespell.tech.midtermassignment01;
 
+
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.AutoCompleteTextView;
+import android.widget.ListView;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class MainActivity extends AppCompatActivity implements AdapterView.OnItemSelectedListener  {
+
+
+    private AutoCompleteTextView AutoComp;
+    private ArrayAdapter<String> Adapt;
+    ListView mListView;
+    String[] Countries;
+    ArrayList<String> itemList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,35 +30,31 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+        String[] country = getResources().getStringArray(R.array.Nations);
+        Adapt = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, country);
+
+        AutoComp = (AutoCompleteTextView) findViewById(R.id.autoComplete);
+        AutoComp.setAdapter(Adapt);
+        AutoComp.setThreshold(1);
+
     }
 
     @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
+    public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+
+        TextView mText = (TextView) view;
+        itemList=new ArrayList<String>(Arrays.asList(Countries));
+        ListView listV=(ListView)findViewById(R.id.listView);
+        listV.setAdapter(Adapt);
+
+        String Items = AutoComp.getText().toString();
+        itemList.add(mText.toString());
+        Adapt.notifyDataSetChanged();
+
     }
 
     @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
+    public void onNothingSelected(AdapterView<?> parent) {
 
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
     }
 }
